fda612d38068e116019486c2c54da016b259bb15
reid holmes <rtholmes@gmail.com>
2010-07-13 11:52:35 -0700

    fixed listener bug

--- src/crystal/Constants.java
+++ src/crystal/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
 	// 90 second timer delay
 	public static int TIMER_CONSTANT = 90000;
 
+	public static final boolean DEBUG_RUNIT = false;
+	public static final boolean DEBUG_UI = false;
 }
