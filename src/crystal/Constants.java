c3500e117a86c3c2b7a1009e557e1a34ff4263fd
reid holmes <rtholmes@gmail.com>
2010-07-13 13:01:04 -0700

    make enable/disable update the timer

--- src/crystal/Constants.java
+++ src/crystal/Constants.java
@@ -3,7 +3,7 @@ package crystal;
 public class Constants {
 
 	// 90 second timer delay
-	public static int TIMER_CONSTANT = 90000;
+	public static int TIMER_CONSTANT = 30000;
 
 	public static final boolean DEBUG_RUNIT = false;
 	public static final boolean DEBUG_UI = false;
