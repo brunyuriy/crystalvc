c5dd1e29a6aff78f21f3c89da1f31fed547b21e4
reid holmes <rtholmes@gmail.com>
2010-07-15 17:56:27 -0700

    update file format (again); add /Users/rtholmes var

--- src/crystal/Constants.java
+++ src/crystal/Constants.java
@@ -1,5 +1,7 @@
 package crystal;
 
+import org.apache.log4j.Level;
+
 public class Constants {
 
 	public static long TIMER_CONSTANT = 10 * 60 * 1000; // 10 mins default
@@ -8,5 +10,12 @@ public class Constants {
 	public static final boolean DEBUG_RUNIT = false;
 	public static final boolean DEBUG_UI = false;
 
+	/**
+	 * When false the Log4J console appender is quiet only reporting ERROR and above.
+	 */
 	public static final boolean QUIET_CONSOLE = true;
+
+	public static final Level LOG_LEVEL = Level.INFO;
+
+	public static final String HOME = "$HOME";
 }
