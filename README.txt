e2cb3ccc0c820b72a04fd9c037bd8800a58574d7
Yuriy Brun <brun@cs.washington.edu>
2010-07-31 14:18:40 -0700

    Fixed the bug that kept adding / to the end of the temp path.

---
+++ README.txt
@@ -0,0 +1,15 @@
+ConflictClient
+
+To Launch:
+
+java -jar crystal.jar
+
+Requirements:
+
+- Mercurial version 1.6 or greater (hg --version to check).
+- Mercurial fetch extension enabled. To enable this you can add:
+
+[extensions]
+fetch=
+
+to your ~/.hgrc file (or Mercurial.ini on Windows).
---
+++ src/crystal/model/DataSource.java
@@ -0,0 +1,75 @@
+package crystal.model;
+
+/**
+ * Describes a repository.
+ * 
+ * @author rtholmes & brun
+ * 
+ */
+public class DataSource {
+
+	public enum RepoKind {
+		GIT, HG
+	}
+
+	// _shortName
+	private String _shortName;
+
+	// the path to the remote repository
+	private String _cloneString;
+
+	// the path to the local clone of the remote repository
+	// private String _localString;
+
+	// the kind of this repo
+	private RepoKind _repoKind;
+
+	public DataSource(String shortName, String cloneString, RepoKind repoKind) {
+		assert shortName != null;
+		assert cloneString != null;
+		// assert localString != null;
+		assert repoKind != null;
+
+		_shortName = shortName;
+		_cloneString = cloneString;
+		// _localString = localString;
+		_repoKind = repoKind;
+	}
+
+	public String getShortName() {
+		return _shortName;
+	}
+
+	public String getCloneString() {
+		return _cloneString;
+	}
+
+	// public String getLocalString() {
+	// return _localString;
+	// }
+
+	public RepoKind getKind() {
+		return _repoKind;
+	}
+
+	public void setKind(RepoKind kind) {
+		_repoKind = kind;
+	}
+
+	public void setShortName(String name) {
+		_shortName = name;
+	}
+
+	public void setCloneString(String name) {
+		_cloneString = name;
+	}
+
+	// public void setLocalString(String name) {
+	// _localString = name;
+	// }
+
+	@Override
+	public String toString() {
+		return getShortName() + "_" + getKind() + "_" + getCloneString();
+	}
+}
