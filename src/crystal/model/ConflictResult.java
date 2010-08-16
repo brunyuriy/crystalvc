60300d0df0ddbbc55ca59a24647ea29ca5f8ed40
Yuriy Brun <brun@cs.washington.edu>
2010-08-16 00:28:56 -0700

    Started writing the When detector.

--- src/crystal/model/ConflictResult.java
+++ src/crystal/model/ConflictResult.java
@@ -96,6 +96,23 @@ public class ConflictResult {
 			}
 			return dominant;
 		}
+		
+		public enum When {
+			NOW, LATER
+		}
+		
+		public static When getWhen(ResultStatus relation, ResultStatus parent) {
+			if (relation == BEHIND) {
+				if (parent == BEHIND)
+					return When.NOW;
+				else if (parent == SAME)
+					return When.LATER;
+				else 
+					return null;
+			}
+//			if ()
+			return null;	
+		}
 
 	}
 
