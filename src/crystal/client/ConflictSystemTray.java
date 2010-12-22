72000844c2336216783da95d8cfb3c12a4092402
Yuriy Brun <brun@cs.washington.edu>
2010-12-22 10:05:08 -0500

    updated the version number.

--- src/crystal/client/ConflictSystemTray.java
+++ src/crystal/client/ConflictSystemTray.java
@@ -45,7 +45,7 @@ public class ConflictSystemTray implements ComputationListener {
 	public static boolean TRAY_SUPPORTED = SystemTray.isSupported();
 
 	// The current Crystal version number.
-	public static String VERSION_ID = "0.1.20110101";
+	public static String VERSION_ID = "0.1.20101222";
 
 	// A pointer to the Crystal window UI.
 	private ConflictClient _client;
