6ac077c935c9f5b2672a1284d42289bdb69f778a
Yuriy Brun <brun@cs.washington.edu>
2010-12-22 02:14:49 -0500

    Cleaned up the documentation and exception handling.

---
+++ src/crystal/model/RevisionHistory.java
@@ -0,0 +1,81 @@
+package crystal.model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import crystal.model.Relationship;
+import crystal.server.GuidanceChecker;
+import crystal.server.HgLogParser;
+import crystal.server.HgLogParser.Checkpoint;
+import crystal.util.SetOperations;
+
+
+public class RevisionHistory {
+	
+	public enum When {
+		NOW, LATER, NOTHING
+	}
+	
+	public enum Capable {
+		MUST, MIGHT, CANNOT, NOTHING
+	}
+	
+	public enum Ease {
+		ME, YOU
+	}
+	
+	public enum Action {
+		CHECKPOINT, RESOLVE, SYNC, PUBLISH, UNKNOWN, NOTHING
+	}
+	
+	private HashMap<String, Checkpoint> _changesets;
+	
+	public RevisionHistory(String log) {
+		_changesets = HgLogParser.parseLog(log);
+	}
+	
+	public int size() {
+		return _changesets.keySet().size();
+	}
+	public String getCommitters (RevisionHistory you) {
+		Set<String> changes = new HashSet<String>();
+		changes = SetOperations.xor(_changesets.keySet(), you._changesets.keySet());
+		
+		Set<Checkpoint> checkpoints = new HashSet<Checkpoint>();
+		for (String current : changes) {
+			if (_changesets.get(current) != null)
+				checkpoints.add(_changesets.get(current));
+			else
+				checkpoints.add(you._changesets.get(current));
+		}
+		
+		return GuidanceChecker.getCommitters(checkpoints);
+	}
+	
+	public When getWhen(RevisionHistory you, RevisionHistory parent, Relationship r) {
+		return GuidanceChecker.getWhen(_changesets.keySet(), you._changesets.keySet(), parent._changesets.keySet(), r);		
+	}
+	
+	public Relationship getConsequences(RevisionHistory you, RevisionHistory parent, Relationship r) {
+		return GuidanceChecker.getConsequences(_changesets.keySet(), you._changesets.keySet(), parent._changesets.keySet(), r);		
+	}
+	
+	public Capable getCapable(RevisionHistory you, RevisionHistory parent, Relationship r, boolean isParent) {
+		return GuidanceChecker.getCapable(_changesets.keySet(), you._changesets.keySet(), parent._changesets.keySet(), r, isParent);
+	}
+	
+	public Ease getEase() {
+		return GuidanceChecker.getEase();
+	}
+	
+	@Override
+	public String toString() {
+		String answer = "";
+		for (String s : _changesets.keySet()) {
+			answer += s + ", ";
+		}
+		return answer.substring(0, answer.length() - 1);
+	}
+
+}
---
+++ src/crystal/util/SetOperations.java
@@ -0,0 +1,57 @@
+package crystal.util;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class SetOperations {
+
+	public static Set<String> intersection(Set<String> a, Set<String> b) {
+		Set<String> all = new HashSet<String>();
+		all.addAll(a);
+		all.addAll(b);
+
+		Set<String> answer = new HashSet<String>();
+
+		for (String current : all) {
+			if (a.contains(current) && b.contains(current))
+				answer.add(current);
+		}
+		return answer;
+	}
+
+	public static Set<String> union(Set<String> a, Set<String> b) {
+		Set<String> answer = new HashSet<String>();
+		answer.addAll(a);
+		answer.addAll(b);
+		return answer;
+	}
+
+	public static Set<String> setDifference(Set<String> a, Set<String> b) {
+		Set<String> answer = new HashSet<String>();
+		answer.addAll(a);
+		answer.removeAll(b);
+		return answer;
+	}
+
+	public static Set<String> xor(Set<String> a, Set<String> b) {
+		Set<String> answer = new HashSet<String>();
+		answer.addAll(setDifference(a,b));
+		answer.addAll(setDifference(b,a));
+		return answer;
+	}
+
+	/*	
+	 * @return true iff a is a subset of b
+	 */
+	public static boolean isSubset(Set<String> a, Set<String> b) {
+		return (setDifference(a, b).isEmpty());
+	}
+	
+	/*	
+	 * @return true iff a is a superset of b
+	 */
+	public static boolean isSuperset(Set<String> a, Set<String> b) {
+		return isSubset(b, a);
+	}
+
+}
