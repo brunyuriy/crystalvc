969d97081ea41cc632716e93fc48bdab3b13ff58
Yuriy Brun <brun@cs.washington.edu>
2011-04-08 00:53:31 -0700

    Removed a warning about an unused library.

--- src/crystal/util/LSMRLogger.java
+++ src/crystal/util/LSMRLogger.java
@@ -14,7 +14,6 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.RollingFileAppender;
-import org.apache.log4j.SimpleLayout;
 import org.apache.log4j.xml.XMLLayout;
 
 /**
