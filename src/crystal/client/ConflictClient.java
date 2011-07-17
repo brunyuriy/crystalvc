f0422470b5e2728a8bfec305a5ecfbd1df24b629
Yuriy Brun <brun@cs.washington.edu>
2011-07-17 11:04:45 -0400

    changed the name daemon

--- src/crystal/client/ConflictClient.java
+++ src/crystal/client/ConflictClient.java
@@ -108,7 +108,7 @@ public class ConflictClient implements ConflictDaemon.ComputationListener {
 
 		_refresh = new JMenuItem("Refresh");
 		JMenuItem editConfiguration = new JMenuItem("Edit Configuration");
-		_disableDaemon = new JMenuItem("Disable Daemon");
+		_disableDaemon = new JMenuItem("Stop Crystal Updates");
 		/*CLEAR CACHE*/
 		_clearCache = new JMenuItem("Clear Cache");
 		JMenuItem exit = new JMenuItem("Exit");
@@ -281,9 +281,9 @@ public class ConflictClient implements ConflictDaemon.ComputationListener {
 
 	public void setDaemonEnabled(boolean enable) {
 		if (enable) {
-			_disableDaemon.setText("Disable Daemon");
+			_disableDaemon.setText("Stop Crystal Updates");
 		} else {
-			_disableDaemon.setText("Enable Daemon");
+			_disableDaemon.setText("Start Crystal Updates");
 		}
 	}
 
