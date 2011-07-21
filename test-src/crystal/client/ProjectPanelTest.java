b0e35e0bc611292db67813abac66ab7c0a19c38a
Haochen Wei <hchwei@uw.edu>
2011-07-21 14:12:13 -0700

    Added test code.

---
+++ test-src/crystal/client/ProjectPanelTest.java
@@ -0,0 +1,19 @@
+package crystal.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProjectPanelTest {
+
+	@Test
+	public void testProjectPanel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+}
