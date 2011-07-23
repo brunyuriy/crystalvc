3bd87e17d7d6742055d6f2e117d9b491e22360ba
Yuriy Brun <brun@cs.washington.edu>
2011-07-23 15:34:12 -0700

    Crystal daemon now stops when you open the config dialog

---
+++ src/crystal/server/HgStateChecker.java
@@ -0,0 +1,448 @@
+package crystal.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+
+import crystal.client.ProjectPreferences;
+import crystal.model.LocalStateResult;
+import crystal.model.Relationship;
+import crystal.model.DataSource;
+import crystal.model.RevisionHistory;
+import crystal.util.RunIt;
+import crystal.util.TimeUtility;
+import crystal.util.RunIt.Output;
+
+/**
+ * Performs hg operations to compute the states of hg repositories.  
+ * Acts as the hg back end for Crystal.
+ * 
+ * @author brun 
+ */
+public class HgStateChecker {
+
+	/**
+	 * @param String pathToHg: the path to the hg executable
+	 * @param String pathToRepo: the full path to the remote repo
+	 * @param String tempWorkPath: path to a temp directory
+	 * @return: Whether or not the pathToRepo is a valid hg repository
+	 */
+	public static boolean isHGRepository(String pathToHg, String pathToRepo, String tempWorkPath) throws IOException {
+		Assert.assertNotNull(pathToHg);
+		Assert.assertNotNull(pathToRepo);
+		Assert.assertNotNull(tempWorkPath);
+
+		String[] myArgs = { "clone", pathToRepo };
+		String output = (RunIt.execute(pathToHg, myArgs, tempWorkPath + "status_check", false)).getOutput();
+
+		RunIt.deleteDirectory(new File(tempWorkPath + "status_check"));
+
+		return (output.indexOf("does not appear to be an hg repository!") < 0);
+	}
+
+	/**
+	 * @param String pathToHg: the path to the hg executable
+	 * @param String pathToRemoteRepo: the full path to the remote repo
+	 * @param String pathToLocalRepo: the path to the local repo which this method creates
+	 * @param String tempWorkPath: path to a temp directory
+	 * @effect: clones the pathToRemoteRepo repository to pathToLocalRepo
+	 */
+	private static synchronized void createLocalRepository(String pathToHg, String pathToRemoteRepo, String pathToLocalRepo, String tempWorkPath, String remoteHg)
+	throws IOException, HgOperationException {
+		Assert.assertNotNull(pathToHg);
+		Assert.assertNotNull(pathToRemoteRepo);
+		Assert.assertNotNull(pathToLocalRepo);
+		Assert.assertNotNull(tempWorkPath);
+
+		// String hg = prefs.getClientPreferences().getHgPath();
+
+		// String tempWorkPath = prefs.getClientPreferences().getTempDirectory();
+		// String pathToRemoteHGRepo = prefs.getEnvironment().getCloneString();
+		// String pathToLocalHGRepo = prefs.getClientPreferences().getTempDirectory() +
+		// prefs.getEnvironment().getLocalPath();
+
+		String command = pathToHg + " clone"; 
+	
+		List<String> myArgsList = new ArrayList<String>();
+		myArgsList.add("clone");
+		if (remoteHg != null) { 
+			myArgsList.add("--remotecmd");
+			myArgsList.add(remoteHg);
+			command += " --remotecmd " + remoteHg; 
+		}
+		myArgsList.add(pathToRemoteRepo);
+		myArgsList.add(pathToLocalRepo);
+		command += " " + pathToRemoteRepo + " " + pathToLocalRepo; 
+		
+		Output output = RunIt.execute(pathToHg, myArgsList.toArray(new String[0]), tempWorkPath, false);
+
+		if (output.getOutput().indexOf("updating to branch") < 0) {
+			String errorMsg = "Crystal tried to execute command:\n" +
+			"\"" + pathToHg + " clone " + pathToRemoteRepo + " " + pathToLocalRepo + "\"\n" +
+			"from \"" + tempWorkPath + "\"\n" +
+			"but got the unexpected output:\n" + 
+			output.toString();
+		//	JOptionPane.showMessageDialog(null, dialogMsg, "hg clone failure", JOptionPane.ERROR_MESSAGE);
+			throw new HgOperationException(errorMsg, tempWorkPath, output.toString());
+		//			throw new RuntimeException("Could not clone repository " + pathToRemoteRepo + " to " + pathToLocalRepo + "\n" + output);
+		}
+	}
+
+	/**
+	 * @param String pathToHg: the path to the hg executable
+	 * @param String pathToLocalRepo: the path to the local repo which this method creates
+	 * @param String tempWorkPath: path to a temp directory
+	 * @return 
+	 * @effect: performs a pull and update on the pathToLocalRepo repository
+	 */
+	private static synchronized  void updateLocalRepository(String pathToHg, String pathToLocalRepo, String pathToRemoteRepo, String tempWorkPath, 
+														   String remoteHg) throws IOException, HgOperationException {
+		Assert.assertNotNull(pathToHg);
+		Assert.assertNotNull(pathToLocalRepo);
+		Assert.assertNotNull(tempWorkPath);
+
+		String command = pathToHg + " pull -u " + pathToRemoteRepo;
+		List<String> myArgsList = new ArrayList<String>();
+		myArgsList.add("pull");
+		myArgsList.add("-u");
+		myArgsList.add(pathToRemoteRepo);
+		if (remoteHg != null) { 
+			myArgsList.add("--remotecmd");
+			myArgsList.add(remoteHg);
+			command += "--remotecmd " + remoteHg; 
+		}
+
+//		String[] myArgs = { "pull", "-u" };
+		Output output = RunIt.execute(pathToHg, myArgsList.toArray(new String[0]), pathToLocalRepo, false);
+
+		if ((output.getOutput().indexOf("files updated") < 0) && (output.getOutput().indexOf("no changes found") < 0))
+			throw new HgOperationException(command, pathToLocalRepo, output.toString());
+	}
+	
+	/**
+	 * Pulls into the local repo and checks for an error in the cache.  
+	 * @param ds: the repo to pull into
+	 * @param hg: path to hg executable
+	 * @param localRepo: path to the local copy of the repo
+	 * @param tempWorkPath: the temp path
+	 * @param remoteHg: the optional remoteHg command (null if none)
+	 * @param repoName: the name of the repo
+	 * @param projectName: the name of the project
+	 * @throws HgOperationException
+	 * @throws IOException
+	 */
+	private static void updateLocalRepositoryAndCheckCacheError(DataSource ds, String hg, String localRepo, String tempWorkPath, String remoteHg, 
+																String repoName, String projectName) throws HgOperationException, IOException {
+		Logger log = Logger.getLogger(HgStateChecker.class);
+		if (new File(localRepo).exists()) {
+			try {
+				updateLocalRepository(hg, localRepo, ds.getCloneString(), tempWorkPath, remoteHg);
+			} catch (HgOperationException e) {
+				String errorMsg = "Crystal is having trouble executing\n" + e.getCommand() + "\nin " +
+				e.getPath() + "\n for your " + repoName + " repository of project " + 
+				projectName + ".\n" + 
+				"Crystal got the unexpected output:\n" + 
+				e.getOutput() + "\n";
+				log.error(errorMsg);
+				errorMsg += "Sometimes, clearing Crystal's local cache can remedy this problem";
+				throw new HgOperationException(errorMsg, e.getPath(), e.getOutput());
+			}
+		} else {
+			createLocalRepository(hg, ds.getCloneString(), localRepo, tempWorkPath, remoteHg);
+		}
+	}
+	
+	/**
+	 * @param prefs: the ProjectPreferences for the project to consider
+	 * @return the local state of my repo of the prefs project
+	 * @throws IOException
+	 */
+	public static String getLocalState(ProjectPreferences prefs) throws IOException {
+		
+		Assert.assertNotNull(prefs);
+		
+		// if source are disabled, return null.
+		if (!prefs.getEnvironment().isEnabled())
+			return null;
+
+		/*
+		 * We are going to:
+		 * 1.  update the local clone
+		 * 2.  get the log from the local clone
+		 * 3.  if the cloneString is local, we will also get heads and check for UNCHECKPOINTED
+		 * 4.  check for MUST_RESOLVE or ALL_CLEAR and return
+		 */
+		
+		String hg = prefs.getClientPreferences().getHgPath();
+		String tempWorkPath = prefs.getClientPreferences().getTempDirectory();
+		String mine = prefs.getProjectCheckoutLocation(prefs.getEnvironment());
+
+		
+		// Step 1. Update the local clone.  If cloning fails, return ERROR state
+		// TODO: The errors on the local state are not reported as toottips yet (need to make LocalState not interned)
+		try {
+			updateLocalRepositoryAndCheckCacheError(prefs.getEnvironment(), hg, mine, tempWorkPath, prefs.getEnvironment().getRemoteHg(), 
+					"your own", prefs.getEnvironment().getShortName());
+		} catch (HgOperationException e) {
+			return LocalStateResult.ERROR + " " + e.getMessage();
+		} catch (IOException e) {
+			return LocalStateResult.ERROR + " " + e.getMessage();
+		}
+		
+		// Step 2. Get the log from the local clone and set the history
+		String[] logArgs = { "log" };
+		Output output = RunIt.execute(hg, logArgs, mine, false);
+		prefs.getEnvironment().setHistory(new RevisionHistory(output.getOutput()));
+		
+		// TODO Step 2.5.  If the history has changed, find out if build or test fails.
+		
+		if ((new File(prefs.getEnvironment().getCloneString())).exists()) {
+			/*
+			 * Check if repo status has non-empty response.  If it does, return UNCHECKPOINTED
+			 */
+			String[] statusArgs = { "status" };
+			output = RunIt.execute(hg, statusArgs , prefs.getEnvironment().getCloneString(), false);
+			// check if any of the lines in the output don't start with "?"
+			StringTokenizer tokens = new StringTokenizer(output.getOutput().trim(), "\n");
+			while (tokens.hasMoreTokens()) {
+				String nextToken = tokens.nextToken();
+				//System.out.println(prefs.getEnvironment().getCloneString() + "#" + nextToken + "#");
+				if (!(nextToken.startsWith("?")))
+					return LocalStateResult.UNCHECKPOINTED;
+			}
+		}
+		
+		// We can't find out the status, but we can find out if MUST_RESOLVE 
+
+		/*
+		 * Check if mine is two headed.  If it is, return MUST_RESOLVE
+		 */
+		String[] headArgs = { "heads" };
+		output = RunIt.execute(hg, headArgs, mine, false);	
+		if (hasTwoHeads(output))
+			return LocalStateResult.MUST_RESOLVE;
+		return LocalStateResult.ALL_CLEAR;
+	}
+	
+	/**
+	 * @param output: the output of "hg heads"
+	 * @return true iff the output indicated there are two heads.
+	 * known problem: if the log message looks like the output of a changelog in the "hg log" command, this method gets confused.
+	 */
+	private static boolean hasTwoHeads(Output output) {
+		Pattern heads = Pattern.compile(".*^changeset: .*^changeset: .*", Pattern.DOTALL | Pattern.MULTILINE);
+		Matcher matcher = heads.matcher(output.getOutput());
+		return matcher.matches();
+	}
+
+	/**
+	 * Computes the Relationship of my repo and one other source repo
+	 * @param prefs: the ProjectPreferences for the project to consider.
+	 * @param source: the repo to compare to.
+	 * @param oldRelationship: the old Relationship, in String form.
+	 * @return the current relationship between my repo in prefs and source
+	 * @throws IOException
+	 */
+	public static String getRelationship(ProjectPreferences prefs, DataSource source, String oldRelationship) {
+
+		Assert.assertNotNull(prefs);
+		Assert.assertNotNull(source);
+
+		Logger log = Logger.getLogger(HgStateChecker.class);
+
+		// if project or source are disabled, return null.
+		if ((!prefs.getEnvironment().isEnabled()) || (!source.isEnabled()))
+			return null;
+
+		String mine = prefs.getProjectCheckoutLocation(prefs.getEnvironment());
+		String yours = prefs.getProjectCheckoutLocation(source);
+
+		String hg = prefs.getClientPreferences().getHgPath();
+
+		String tempWorkPath = prefs.getClientPreferences().getTempDirectory();
+		// tempWorkPath + tempMyName used to store a local copy of my repo
+		String tempMyName = "tempMine_" + TimeUtility.getCurrentLSMRDateString();
+		// tempWorkPath + tempYourName used to store a local copy of your repo
+//		String tempYourName = "tempYour_" + TimeUtility.getCurrentLSMRDateString();
+		
+		// My local copy has already been updated when we checked the local status
+		// So we are just going to update yours
+		try {
+			updateLocalRepositoryAndCheckCacheError(source, hg, yours, tempWorkPath, source.getRemoteHg(), 
+					source.getShortName(), prefs.getEnvironment().getShortName());
+		} catch (HgOperationException e1) {
+			return Relationship.ERROR + " " + e1.getMessage();
+		} catch (IOException e2) {
+		    return Relationship.ERROR + " " + e2.getMessage();
+		}
+
+		// Get your log and set your history
+		String[] logArgs = { "log" };
+		Output logOutput;
+		try {
+		    logOutput = RunIt.execute(hg, logArgs, yours, false);
+		} catch (IOException e2) {
+            return Relationship.ERROR + " Couldn't get the log: " + e2.getMessage();
+        }
+
+		RevisionHistory yourHistory = new RevisionHistory(logOutput.getOutput());
+		source.setHistory(yourHistory);
+
+		RevisionHistory myHistory = prefs.getEnvironment().getHistory();
+
+		if (myHistory == null)
+		    return Relationship.ERROR + " Could not parse the history of your repository.";
+				
+		// TODO figure out if we need to check for compile and test whenever histories change: 
+		// one of (source.hasHistoryChanged()) or (prefs.getEnvironment.hasHistoryChanged()) are true
+
+		if (myHistory.equals(yourHistory))
+			return Relationship.SAME;
+		
+		else if (myHistory.superHistory(yourHistory))
+			return Relationship.AHEAD;
+		
+		else if (myHistory.subHistory(yourHistory))
+			return Relationship.BEHIND;
+		
+		// Well, we in one of {MERGE, CONFLICT, COMPILECONFLICT, TESTCONFLICT} relationships, so we are going to have to bite the bullet and make local clones.  
+		
+		// First, check if anything has changed.
+		if (!(prefs.getEnvironment().hasHistoryChanged() || source.hasHistoryChanged())) {
+			// Nothing has changed.  Keep old status.
+			return oldRelationship;
+		}
+		// OK, things have changed.  We have to recompute.  
+		String answer;
+		Output output;
+		
+		// pull your repo into [a temp clone of] mine
+		String[] myArgs = { "clone", mine, tempMyName };
+		try {
+		    output = RunIt.execute(hg, myArgs, tempWorkPath, false);
+		} catch (IOException e2) {
+            return Relationship.ERROR + " Couldn't make a temp clone: " + e2.getMessage();
+        }
+		String[] pullArgs = { "pull", yours };
+		try {
+		    output = RunIt.execute(hg, pullArgs, tempWorkPath + tempMyName, false);
+		} catch (IOException e2) {
+            return Relationship.ERROR + " Couldn't pull into my temp clone: " + e2.getMessage();
+        }
+		
+		if (output.getOutput().indexOf("(run 'hg heads' to see heads, 'hg merge' to merge)") >= 0) {
+			// there are two heads, so let's see if they merge cleanly
+			String[] mergeArgs = { "merge", "--noninteractive" };
+			try {
+			    output = RunIt.execute(hg, mergeArgs, tempWorkPath + tempMyName, false);
+			} catch (IOException e2) {
+	            return Relationship.ERROR + " Couldn't execute merge: " + e2.getMessage();
+	        }
+			// if the merge goes through cleanly, we can try to compile and test
+			if (output.getOutput().indexOf("(branch merge, don't forget to commit)") >= 0) {
+				// try to compile
+				String compileCommand = prefs.getEnvironment().getCompileCommand();
+				//System.out.println(compileCommand);
+				if (compileCommand != null) {
+				    try {
+				        Output compileOutput = RunIt.tryCommand(compileCommand, tempWorkPath + tempMyName);
+				        if (compileOutput.getStatus() != 0)
+				            // if unsuccessful:
+				            answer = Relationship.COMPILECONFLICT;
+				        else {
+				            // if successful try to test
+				            String testCommand = prefs.getEnvironment().getTestCommand();
+				            if (testCommand != null) {
+				                Output testOutput = RunIt.tryCommand(testCommand, tempWorkPath + tempMyName);
+				                if (testOutput.getStatus() != 0)
+				                    // if unsuccessful:
+				                    answer = Relationship.TESTCONFLICT;
+				                else
+				                    // if successful:
+				                    answer = Relationship.MERGECLEAN;
+				            }
+				            else
+				                // we don't know how to test
+				                answer = Relationship.MERGECLEAN;
+				        }
+                    } catch (IOException e2) {
+                        return Relationship.ERROR + " Had and IO error trying to either compile or run tests: " + e2.getMessage();
+                    }
+				}
+				else
+					// we don't know how to compile
+					answer = Relationship.MERGECLEAN;
+			}
+			// otherwise, the merge failed
+			else
+				answer = Relationship.MERGECONFLICT;
+		} else {
+			// something went wrong
+			log.error("Crystal is having trouble comparing" + mine + " and " + yours + "\n" + output.toString());
+			String errorMsg = "Crystal is having trouble comparing\n" + 
+			mine + " and " + yours + "\n" + 
+			"for the repository " + source.getShortName() + " in project " + prefs.getEnvironment().getShortName() + ".\n";
+			// JOptionPane.showConfirmDialog(null, dialogMsg);
+			// source.setEnabled(false);
+			return Relationship.ERROR + " " + errorMsg;
+		}
+		// Clean up temp directories:
+		RunIt.deleteDirectory(new File(tempWorkPath + tempMyName));
+		return answer;
+	}
+
+	/**
+	 * An exception for HgOperations
+	 */
+	public static class HgOperationException extends Exception {
+		private static final long serialVersionUID = -6885233021486785003L;
+		
+		private String _output;
+		private String _command;
+		private String _path;
+
+		/**
+		 * Creates a new HgOperationException 
+		 * @param command: the hg command that caused the exception.
+		 * @param path: the path from which the command was run.
+		 * @param output: the output of the command.
+		 */
+		public HgOperationException(String command, String path, String output) {
+			super("Tried to execute \n\"" + command + "\"\n in \"" + path + "\"\n" +
+					"but got the output\n" + output);
+			_output = output;
+			_path = path;
+			_command = command;
+		}
+		
+		/**
+		 * @return the output of the command.
+		 */
+		public String getOutput() {
+			return _output;
+		}
+		
+		/**
+		 * @return the path in which this command was run.
+		 */
+		public String getPath() {
+			return _path;
+		}
+
+		/**
+		 * @return the command that was run.
+		 */
+		public String getCommand() {
+			return _command;
+		}
+
+	}
+}
+
---
+++ test-src/crystal/util/SpringLayoutUtilityTest.java
@@ -0,0 +1,35 @@
+package crystal.util;
+
+import static org.junit.Assert.*;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import org.junit.Test;
+
+/**
+ * Class SpringLayoutUtilityTest will test the performance of class
+ * SpringLayoutUtility
+ * @author Haochen
+ *
+ */
+
+public class SpringLayoutUtilityTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidPanelParameter(){
+		JPanel temp = new JPanel(new BorderLayout());
+		SpringLayoutUtility.formGrid(temp, 2, 2);
+	}
+
+	
+	@Test
+	public void testFormGrid() {
+		JPanel temp = new JPanel(new SpringLayout());
+		SpringLayoutUtility.formGrid(temp, 2, 2);
+		//TODO
+	}
+
+}
