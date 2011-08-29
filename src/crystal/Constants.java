670c2b5efad8fa45f7da01843dc1695e05719d37
Yuriy Brun <brun@cs.washington.edu>
2011-08-28 22:52:59 -0700

    Added Haochen to the list of contributors

---
+++ src/crystal/Constants.java
@@ -0,0 +1,57 @@
+package crystal;
+
+/**
+ * Constants contains several constants for Crystal to use.  
+ * 
+ * @author brun
+ * @author rtholmes
+ */
+
+import org.apache.log4j.Level;
+
+public class Constants {
+
+	/**   
+	 * TIMER_CONSTANT is Crystal's refresh period, in milliseconds.
+	 * Crystal will attempt to refresh the local state, actions, relationships, and guidance every
+	 *   TIMER_CONSTANT milliseconds.  
+	 */
+	
+	// the refresh time in seconds
+	public static long DEFAULT_REFRESH = 10 * 60; // 10 mins default
+	// public static long REFRESH = DEFAULT_REFRESH;
+	// public static long TIMER_CONSTANT = 10 * 60 * 1000; // 10 mins default
+	// for demos:
+	//	public static long TIMER_CONSTANT = 1000; 
+	// for debugging:
+	// public static long TIMER_CONSTANT = 5 * 1000; // 5 seconds
+	
+	/**
+     * Minimum hg version
+     * may only contain 1 decimal point.
+     */
+	public static final double MIN_HG_VERSION = 1.6;
+
+	/**
+	 * Debug constants.
+	 */
+	public static final boolean DEBUG_RUNIT = false;
+	public static final boolean DEBUG_UI = false;
+
+	/**
+	 * Log constants.
+	 */
+	/**
+	 * When false the Log4J console appender is quiet only reporting ERROR and above.
+	 */
+	public static final boolean QUIET_CONSOLE = true;
+	/**
+	 * The level of logging.
+	 */
+	public static final Level LOG_LEVEL = Level.INFO;
+
+	/**
+	 * HOME is the string that designates the home directory in the local file system.
+	 */
+	public static final String HOME = "$HOME";
+}
---
+++ test-src/crystal/model/RelationshipTest.java
@@ -0,0 +1,96 @@
+package crystal.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Test;
+
+import crystal.model.DataSource.RepoKind;
+import crystal.model.RevisionHistory.Action;
+
+/**
+ * Class RelationshipTest will test the performance of class Relationship
+ * 
+ * @author Haochen
+ *
+ */
+
+public class RelationshipTest {
+	public static final String ERROR = "ERROR error message";
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullInputConstructor(){
+		new Relationship(null, null, null);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testStrangeNameConstructor() {
+		new Relationship("abc", null, null);
+	}
+	
+	@Test
+	public void testConstructor(){
+		Relationship errorName = new Relationship(Relationship.ERROR + " error message", null, null);
+		assertTrue("error message", errorName.getToolTipText().equals("error message"));
+		assertTrue("error name", errorName.getName().equals(Relationship.ERROR));
+		
+		
+		Relationship sameName = new Relationship("Same", null, null);
+		assertTrue("same name to upper case", sameName.getName().equals(Relationship.SAME));
+		assertFalse("Is not ready", sameName.isReady());
+		assertNotNull("Set Icon", sameName.getIcon());
+		assertNotNull("Set Image", sameName.getImage());
+	}
+
+	@Test
+	public void testCalculateAction() {
+		Relationship temp1 = new Relationship(Relationship.AHEAD, null, null);
+		assertNull("Call getAction before calling calculateAction", temp1.getAction());
+		
+		temp1.calculateAction(LocalStateResult.PENDING, new Relationship(Relationship.AHEAD, null, null));
+		assertTrue("After calculating action", temp1.getAction().equals(Action.UNKNOWN));
+		
+		temp1.calculateAction(null, new Relationship(Relationship.AHEAD, null, null));
+		assertNull("When localState is null", temp1.getAction());
+	}
+
+
+	@Test
+	public void testGetActionRepoKind() {
+		Relationship temp1 = new Relationship(Relationship.AHEAD, null, null);
+		
+		assertTrue("Get action when action is null", temp1.getAction(RepoKind.HG).equals("cannot compute hg action"));
+		
+		temp1.calculateAction(null, null);
+		assertTrue("Get action when action is UNKNOWN", temp1.getAction(RepoKind.HG).equals("not computed"));
+		
+		temp1.calculateAction("", new Relationship(Relationship.SAME, null, null));
+		assertNull("Get action when action is NOTHING", temp1.getAction(RepoKind.HG));
+	}
+
+
+	@Test
+	public void testGetDominant() {
+		assertNull("Null input", Relationship.getDominant(null));
+		Set<Relationship> temp1 = new TreeSet<Relationship>();
+		assertNull("Empty relationships collection", Relationship.getDominant(temp1));
+	}
+
+	@Test
+	public void testEqualsObject() {
+		Relationship temp = new Relationship(Relationship.AHEAD, null, null);
+		assertFalse("Compare with null", temp.equals(null));
+		assertFalse("Compare with other object", temp.equals("temp"));
+		
+		Relationship temp2 = new Relationship(Relationship.ERROR, null, null);
+		assertFalse("Compare with different name relationship", temp.equals(temp2));
+		
+		Relationship temp3 = new Relationship(Relationship.ERROR, null, null);
+		assertTrue("Compare with same name relationship", temp2.equals(temp3));
+
+	}
+
+}
