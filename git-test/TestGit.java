3141443aef87b805af8776e4b1e2d5af67317d2d
Haochen Wei <hchwei@uw.edu>
2011-11-01 00:32:49 -0700

    updated TestGit.java

--- git-test/TestGit.java
+++ git-test/TestGit.java
@@ -7,20 +7,21 @@ public class TestGit {
 	
 	private static String[] paths = new String[5];
 	static {
-		paths[0] = "C:\\Users\\Haochen\\Dropbox\\gittestproject\\temp\\me";
-		paths[1] = "C:\\Users\\Haochen\\Dropbox\\gittestproject\\temp\\same";
-		paths[2] = "C:\\Users\\Haochen\\Dropbox\\gittestproject\\temp\\behind";
-		paths[3] = "C:\\Users\\Haochen\\Dropbox\\gittestproject\\temp\\ahead";
-		paths[4] = "C:\\Users\\Haochen\\Dropbox\\gittestproject\\temp\\merge";
+		paths[0] = "\\Dropbox\\gittestproject\\temp\\me";
+		paths[1] = "\\Dropbox\\gittestproject\\temp\\same";
+		paths[2] = "\\Dropbox\\gittestproject\\temp\\behind";
+		paths[3] = "\\Dropbox\\gittestproject\\temp\\ahead";
+		paths[4] = "\\Dropbox\\gittestproject\\temp\\merge";
 	}
 	public static void main(String[] args) throws IOException{
 		
 		PrintStream out = new PrintStream("output.txt");
 		String[] logArgs = {"log"};
 		String executablePath = "C:\\Program Files\\Git\\cmd\\\\git.cmd";
+		String homepath = System.getProperty("user.home");
 		for (String path : paths) {
-			Output output = RunIt.execute(executablePath, logArgs, path, false);
-			out.println("directory: " + path + "\noutput: \n" + output.getOutput());
+			Output output = RunIt.execute(executablePath, logArgs, homepath + path, false);
+			out.println("directory: " + homepath + path + "\noutput: \n" + output.getOutput());
 			out.println("error: \n" + output.getError());
 		}
 	}
