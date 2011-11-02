ee5ff50034620a5511aff73f777189b564f3903f
Yuriy Brun <brun@cs.washington.edu>
2011-11-02 05:12:12 -0700

    modified the git test code to be more modular and added proper paths for windows

--- git-test/TestGit.java
+++ git-test/TestGit.java
@@ -6,22 +6,24 @@ import java.io.PrintStream;
 public class TestGit {
 	
 	private static String[] paths = new String[5];
+	private static String pathToDropbox = "C:\\Users\\Yuriy\\Desktop\\work\\My Dropbox\\";
 	static {
-		paths[0] = "\\Dropbox\\gittestproject\\temp\\me";
-		paths[1] = "\\Dropbox\\gittestproject\\temp\\same";
-		paths[2] = "\\Dropbox\\gittestproject\\temp\\behind";
-		paths[3] = "\\Dropbox\\gittestproject\\temp\\ahead";
-		paths[4] = "\\Dropbox\\gittestproject\\temp\\merge";
+		paths[0] = pathToDropbox + "gittestproject\\temp\\me";
+		paths[1] = pathToDropbox + "gittestproject\\temp\\same";
+		paths[2] = pathToDropbox + "gittestproject\\temp\\behind";
+		paths[3] = pathToDropbox + "gittestproject\\temp\\ahead";
+		paths[4] = pathToDropbox + "gittestproject\\temp\\merge";
 	}
 	public static void main(String[] args) throws IOException{
 		
 		PrintStream out = new PrintStream("output.txt");
 		String[] logArgs = {"log"};
-		String executablePath = "C:\\Program Files\\Git\\cmd\\\\git.cmd";
-		String homepath = System.getProperty("user.home");
+//		String executablePath = "C:\\Program Files (x86)\\Git\\cmd\\\\git.cmd";
+		String executablePath = "C:\\Program Files (x86)\\Git\\bin\\\\git.exe";
+		// String homepath = System.getProperty("user.home");
 		for (String path : paths) {
-			Output output = RunIt.execute(executablePath, logArgs, homepath + path, false);
-			out.println("directory: " + homepath + path + "\noutput: \n" + output.getOutput());
+			Output output = RunIt.execute(executablePath, logArgs, path, false);
+			out.println("directory: " + path + "\noutput: \n" + output.getOutput());
 			out.println("error: \n" + output.getError());
 		}
 	}
